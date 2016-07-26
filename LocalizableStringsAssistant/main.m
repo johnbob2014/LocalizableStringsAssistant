e2f5dfad97fefaa404d88b8565482627d26917d2
johnbob2014 <johnbob2014@icloud.com>
2016-07-26 09:17:20 +0800

    Initial Commit

---
+++ LocalizableStringsAssistant/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LocalizableStringsAssistant
+//
+//  Created by BobZhang on 16/7/26.
+//  Copyright © 2016年 ZhangBaoGuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
